8f2d21aecf29ea1a1a1008d70da3404baa55dc5b
miguel <miguel>
2026-01-13 22:43:27 -0500

    Commit inicial

---
+++ Semana1/Semana1/Controllers/RegistroController.cs
@@ -0,0 +1,39 @@
+﻿using Semana1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semana1.Controllers
+{
+    public class RegistroController : Controller
+    {
+        // GET: Registro
+        public ActionResult Index()
+        {
+            Registro registro = new Registro();
+            ViewBag.fechaRegistro = registro.fechaRegistro;
+            return View();
+        }
+
+        public ActionResult MostrarRegistro()
+        {
+            Registro registro = new Registro();
+            ViewBag.nombreCompleto = registro.nombreCompleto();
+            return View(registro);
+        }
+
+        [HttpGet]
+        public ActionResult GuardarRegistro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GuardarRegistro(Registro registro)
+        {
+            return View();
+        }
+    }
+}
---
+++ Semana1/Semana1/Models/Registro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Semana1.Models
+{
+    public class Registro
+    {
+        public string codigo {  get; set; }
+        public DateTime fechaRegistro { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public int edad {  get; set; }
+
+        public Registro()
+        {
+            codigo = "241";
+            fechaRegistro = DateTime.Today;
+            nombre = "Miguel";
+            apellido = "Berrio";
+            edad = 0;
+        }
+
+        public string nombreCompleto()
+        {
+            return nombre+ " "+ apellido;
+        }
+    }
+}
