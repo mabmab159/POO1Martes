f0e07f332e13b739044c4f11932f1e747e32a0b0
miguel <miguel>
2026-01-20 22:45:05 -0500

    Clase 2 Final

--- Semana1/Semana1/Controllers/RegistroController.cs
+++ Semana1/Semana1/Controllers/RegistroController.cs
@@ -9,12 +9,16 @@ namespace Semana1.Controllers
 {
     public class RegistroController : Controller
     {
+
+        static List<Registro> listaRegistros = new List<Registro>() { new Registro(), new Registro()};
+
+        Singleton singleton = Singleton.GetInstance(); //Crear la instancia
+        Singleton singleton2 = Singleton.GetInstance(); //Reutiliza la instancia anterior
+
         // GET: Registro
         public ActionResult Index()
         {
-            Registro registro = new Registro();
-            ViewBag.fechaRegistro = registro.fechaRegistro;
-            return View();
+            return View(listaRegistros);
         }
 
         public ActionResult MostrarRegistro()
@@ -24,16 +28,62 @@ namespace Semana1.Controllers
             return View(registro);
         }
 
-        [HttpGet]
+        [HttpGet] // Crear la vista del formulario
         public ActionResult GuardarRegistro()
         {
             return View();
         }
 
-        [HttpPost]
+        [HttpPost] //Esto sirve para cuando envie el formulario | Click en enviar o guardar
         public ActionResult GuardarRegistro(Registro registro)
         {
+            listaRegistros.Add(registro);
+            ViewBag.mensaje = "Registro guardado correctamente";
             return View();
+        }
+
+        public ActionResult Details(string codigoRegistro)
+        {
+            //Busque el codigo en la lista y luego lo muestre en la vista
+            Registro registroEncontrado = listaRegistros.Find(e => e.codigo.Equals(codigoRegistro));
+            //Retornar la vista con el registro encontrado
+            return View(registroEncontrado);
+        }
+
+        public ActionResult Edit(string codigoRegistro)
+        {
+            //Busque el codigo en la lista y luego lo muestre en la vista
+            Registro registroEncontrado = listaRegistros.Find(e => e.codigo.Equals(codigoRegistro));
+            //Retornar la vista con el registro encontrado
+            return View(registroEncontrado);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Registro registro)
+        {
+            //Buscar el registro en la lista
+            Registro registroEncontrado = listaRegistros.Find(e => e.codigo.Equals(registro.codigo));
+            registroEncontrado.nombre = registro.nombre;
+            registroEncontrado.apellido = registro.apellido;
+            registroEncontrado.edad = registro.edad;
+            ViewBag.mensaje = "Registro actualizado correctamente";
+            return View(registroEncontrado);
+        }
+
+        public ActionResult Delete(string codigoRegistro)
+        {
+            //Busque el codigo en la lista y luego lo muestre en la vista
+            Registro registroEncontrado = listaRegistros.Find(e => e.codigo.Equals(codigoRegistro));
+            //Retornar la vista con el registro encontrado
+            return View(registroEncontrado);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Registro registro)
+        {
+            Registro registroEncontrado = listaRegistros.Find(e => e.codigo.Equals(registro.codigo));
+            listaRegistros.Remove(registroEncontrado);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Semana1/Semana1/Models/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Semana1.Models
+{
+    public class Singleton
+    {
+        private static Singleton singleton;
+        private Singleton() { }
+
+        //Este metodo se encarga de la gestion de la instancia unica
+        public static Singleton GetInstance()
+        {
+            if(singleton == null)
+            {
+                singleton = new Singleton();
+            }
+            return singleton;
+        }
+
+        public string Mensaje()
+        {
+            return "Hola desde el patron Singleton";
+        }
+    }
+}
